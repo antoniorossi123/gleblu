5a9abdc68f767b5bcd4934dde5c47eb78cf27476
antoniorossi123 <33842433+antoniorossi123@users.noreply.github.com>
2021-05-29 13:39:42 +0200

    1

--- layout/scripts/jquery.mobilemenu.js
+++ layout/scripts/jquery.mobilemenu.js
@@ -1,4 +1,4 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/* Assagettare tra mostrando e nascondere i collegamenti del menu di navigazione quando l'utente fa clic sull'icona del menu / barra hamburger*/
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
